c639842d1dc6cc6a14e8ffd3b7df7fa4c983acbb
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-11-15 15:51:48 +0530

    mfapi

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.status(200).send("Welcome to Dominos!");
+    return;
+  } else if (req.url === "/contact") {
+    const contact = {
+      phone: "18602100000",
+      email: "guestcaredominos@jublfood.com"
+    };
+    res.status(200).send(contact);
+    return;
+  } else {
+    res.status(400).send("Invalid Request");
+    return;
+  }
 }
+httpServer.listen(8081);
 
 module.exports = httpServer;
